a424ff0d18818a48b39a5208466a8fa7f424bba7
isuru <isurulakshan607@gmail.com>
2024-06-26 15:01:33 +0530

    user-service completed

--- back-end/user-service/src/main/java/lk/ijse/gdse/userservice/controller/UserController.java
+++ back-end/user-service/src/main/java/lk/ijse/gdse/userservice/controller/UserController.java
@@ -1,15 +1,42 @@
 package lk.ijse.gdse.userservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lk.ijse.gdse.userservice.dto.UserDTO;
+import lk.ijse.gdse.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
-    @GetMapping("/getUser")
-    public String getUser(){
-        return "User Controller";
+    @PostMapping("/save")
+    public UserDTO saveUser(@Valid @RequestBody UserDTO userDTO) {
+        System.out.println("UserDTO = "+userDTO);
+        return userService.saveUser(userDTO);
+    }
+
+    @PatchMapping("/update")
+    public UserDTO updateUser(@Valid @RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/delete")
+    public boolean deleteUser(@RequestParam("userName") String userName){
+        return userService.deleteUser(userName);
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<UserDTO> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/searchByUserName")
+    public UserDTO searchUserByUserName(@RequestParam("userName") String userName){
+        return userService.searchUserByUserName(userName);
     }
 }
